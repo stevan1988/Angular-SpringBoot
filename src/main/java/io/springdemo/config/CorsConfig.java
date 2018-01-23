1000a1858b60a69a86a02765eb45d89ed59a18f2
stodorovic <steva1988@gmail.com>
2018-01-23 15:08:06 +0100

    Testing automatic build on Jenkins

--- src/main/java/io/springdemo/config/CorsConfig.java
+++ src/main/java/io/springdemo/config/CorsConfig.java
@@ -2,7 +2,6 @@ package io.springdemo.config;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
